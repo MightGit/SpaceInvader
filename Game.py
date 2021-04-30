a0e2c6a74945a72d0792a692edce5600fdcc315f
MightGit <74358998+MightGit@users.noreply.github.com>
2021-04-30 20:43:53 +0200

    update

--- Game.py
+++ Game.py
@@ -1,1 +1,186 @@
-class Game
+import pygame
+import math
+import random
+
+from pygame import mixer
+
+
+# start af pygame
+pygame.init()
+
+# screen for spillet
+screen = pygame.display.set_mode((800, 600))
+
+# Baggrund
+background = pygame.image.load('background.jpg')
+
+#baggrunds musik
+mixer.music.load('song.mp3')
+mixer.music.play(-1)
+
+
+
+# Title og Icon
+pygame.display.set_caption("Pirate Invaders")
+icon = pygame.image.load('whatthef.PNG')
+pygame.display.set_icon(icon)
+
+# spiller
+heltImg = pygame.image.load('pirat64.png')
+heltX = 370
+heltY = 480
+heltX_change = 0
+
+# enemy
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy2.png'))
+    enemyX.append(random.randint(0, 734))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(0.1)
+    enemyY_change.append(40)
+
+# skyd
+
+# read = not on screen
+# fire = skyd kommer frem og bevæger
+
+skydImg = pygame.image.load('bullet2.png')
+skydX = 0
+skydY = 480
+skydX_change = 0
+skydY_change = 0.3
+skyd_state = "ready"
+
+# scoreboard
+
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf', 32)
+
+textX = 10
+textY = 10
+
+#gameover text
+over_font = pygame.font.Font('freesansbold.ttf', 64)
+
+def show_score(x, y):
+    score = font.render("Score:" + str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+def game_over_text():
+    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+    screen.blit(over_text, (200, 250))
+
+def helt(x, y):
+    screen.blit(heltImg, (x, y))
+
+
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
+
+
+def fire_skyd(x, y):
+    global skyd_state
+    skyd_state = "fire"
+    screen.blit(skydImg, (x + 16, y + 10))
+
+
+def isCollision(enemyX, enemyY, skydX, skydY):
+    distance = math.sqrt((math.pow(enemyX - skydX, 2)) + math.pow(enemyY - skydY, 2))
+    if distance < 27:
+        return True
+    else:
+        return False
+
+
+# Loop
+running = True
+
+while running:
+
+    # Farver for background
+    screen.fill((0, 0, 0))
+    # baggrund billed
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        # keystroke
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                heltX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                heltX_change = 0.3
+            if event.key == pygame.K_SPACE:
+                if skyd_state is "ready":
+                    skyd_Sound = mixer.Sound('bshot.mp3')
+                    skyd_Sound.play()
+
+                    skydX = heltX
+                    fire_skyd(skydX, skydY)
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                heltX_change = 0
+
+    # boundry
+    heltX += heltX_change
+
+    if heltX <= 0:
+        heltX = 0
+    elif heltX >= 736:
+        heltX = 736
+
+    # enemy movement
+    enemyX += enemyX_change
+
+    for i in range(num_of_enemies):
+
+        #game over
+        if enemyY[i] > 200:
+            for j in range(num_of_enemies):
+                enemyY[j] = 2000
+            game_over_text()
+            break
+
+
+        enemyX[i] += enemyX_change[i]
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 0.1
+            enemyY[i] += enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -0.1
+            enemyY[i] += enemyY_change[i]
+
+        # collision
+        collision = isCollision(enemyX[i], enemyY[i], skydX, skydY)
+        if collision:
+            explosion_Sound = mixer.Sound('whack.mp3')
+            explosion_Sound.play()
+            skydY = 480
+            skyd_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 734)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
+
+    # bullet move
+    if skydY <= 0:
+        skydY = 480
+        skyd_state = "ready"
+
+    if skyd_state is "fire":
+        fire_skyd(skydX, skydY)
+        skydY -= skydY_change
+
+    helt(heltX, heltY)
+    show_score(textX, textY)
+    pygame.display.update()
--- venv/rando.py
+++ venv/rando.py
@@ -1,13 +1,129 @@
-import pygame
+import  pygame
 
-class RocketClass:
+
+class  Game :
+    screen = None
+    aliens = []
+    rockets = []
+    lost = False
+
+    def __init__(self, width, height):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((width, height))
+        self.clock = pygame.time.Clock()
+        done = False
+
+        helt = Helt(self, width / 2, height - 20)
+        generator = Generator(self)
+        rocket = None
+
+        while not done:
+            if len(self.aliens) == 0:
+                self.displayText("VICTORY ACHIEVED")
+
+            pressed = pygame.key.get_pressed()
+            if pressed[pygame.K_LEFT]:  # sipka doleva
+                helt . x  -=  2  if  helt . x  >  20  else  0   # left area boundary
+            elif  pressed [ pygame . K_RIGHT ]:   # right arrow
+                helt.x += 2 if helt.x < width - 20 else 0  # prava hranice
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not self.lost:
+                    self.rockets.append(Rocket(self, helt.x, helt.y))
+
+            pygame.display.flip()
+            self.clock.tick(60)
+            self.screen.fill((0, 0, 0))
+
+            for alien in self.aliens:
+                alien.draw()
+                alien.checkCollision(self)
+                if (alien.y > height):
+                    self.lost = True
+                    self.displayText("YOU DIED")
+
+            for rocket in self.rockets:
+                rocket.draw()
+
+            if not self.lost: helt.draw()
+
+    def displayText(self, text):
+        pygame.font.init()
+        font = pygame.font.SysFont('Arial', 50)
+        textsurface = font.render(text, False, (44, 0, 62))
+        self.screen.blit(textsurface, (110, 160))
+
+
+class  Alien :
     def __init__(self, game, x, y):
         self.x = x
-        self.y = y
+        self.game = game
+        self . y  =  y
+        self.size = 30
+
+    def draw(self):
+        pygame.draw.rect(self.game.screen,
+                         ( 81 , 43 , 88 ),
+                         pygame.Rect(self.x, self.y, self.size, self.size))
+        self.y += 0.05
+
+    def checkCollision(self, game):
+        for rocket in game.rockets:
+            if (rocket.x < self.x + self.size and
+                    rocket.x > self.x - self.size and
+                    rocket.y < self.y + self.size and
+                    rocket.y > self.y - self.size):
+                game.rockets.remove(rocket)
+                game.aliens.remove(self)
+
+
+class Helt:
+    def __init__(self, game, x, y):
+        self.x = x
+        self.game = game
+        self . y  =  y
+
+    def draw(self):
+
+        pygame.draw.rect(self.game.screen,
+                         (210, 0, 251),
+                         pygame.Rect(self.x, self.y, 8, 5))
+
+
+
+class  Generator :
+    def __init__(self, game):
+        margin  =  30   # space from the edge of the screen
+        width  =  50   # space between aliens
+        for x in range(margin, game.width - margin, width):
+            for y in range(margin, int(game.height / 2), width):
+                game.aliens.append(Alien(game, x, y))
+
+        # game.aliens.append(Alien(game, 280, 50))
+
+
+class Rocket:
+    def __init__(self, game, x, y):
+        self.x = x
+        self . y  =  y
         self.game = game
 
     def draw(self):
-        pygame.draw.rect(self.game.screen, 
-                         (254, 52, 110),  
+        pygame.draw.rect(self.game.screen,  # renderovací plocha
+                         ( 254 , 52 , 110 ),   # object color
                          pygame.Rect(self.x, self.y, 2, 4))
-        self.y -= 2  
+        self . y  -=  2   # will fly up 2px / frame on the playing area
+
+
+
+def game_loop():
+    pygame.mixer.music.play(-1)
+
+if __name__ == '__main__':
+    game = Game(800, 800)
+
+
